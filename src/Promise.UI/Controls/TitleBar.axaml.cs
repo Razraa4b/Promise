71766bc58bace4eebc6b42729c993c4c4cd68828
Razraaab <razraaab@gmail.com>
2025-05-22 21:02:37 +0300

    feat(grid splitter fix): GridSplitter now cannot resize a column larger than the window size

--- src/Promise.UI/Controls/TitleBar.axaml.cs
+++ src/Promise.UI/Controls/TitleBar.axaml.cs
@@ -2,13 +2,18 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using Avalonia.ReactiveUI;
+using Avalonia.VisualTree;
+using ReactiveUI;
 using System;
 using System.Threading.Tasks;
 
 namespace Promise.UI.Controls
 {
-    public partial class TitleBar : UserControl
+    public partial class TitleBar : UserControl, IActivatableView
     {
+        private Window? hostWindow;
+
         // Icon Property
         public static readonly DirectProperty<TitleBar, object?> IconProperty = AvaloniaProperty.RegisterDirect<TitleBar, object?>(
             nameof(Icon),
@@ -55,46 +60,36 @@ namespace Promise.UI.Controls
         {
             InitializeComponent();
 
-            SubscribeToWindowState();
-        }
-
-        private async void SubscribeToWindowState()
-        {
-            Window? hostWindow = (Window?)VisualRoot;
-
-            while (hostWindow == null)
+            this.WhenActivated(disposables =>
             {
-                hostWindow = (Window?)VisualRoot;
-                await Task.Delay(50);
-            }
-
-            hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(s =>
-            {
-                if (s != WindowState.Maximized)
+                hostWindow = this.GetVisualRoot() as Window;
+                if(hostWindow != null)
                 {
-                    MaximizeIcon.Data = Geometry.Parse("M2048 2048v-2048h-2048v2048h2048zM1843 1843h-1638v-1638h1638v1638z");
-                    hostWindow.Padding = new Thickness(0);
-                }
-                if (s == WindowState.Maximized)
-                {
-                    MaximizeIcon.Data = Geometry.Parse("M2048 1638h-410v410h-1638v-1638h410v-410h1638v1638zm-614-1024h-1229v1229h1229v-1229zm409-409h-1229v205h1024v1024h205v-1229z");
-                    hostWindow.Padding = new Thickness(7);
+                    hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(s =>
+                    {
+                        if (s != WindowState.Maximized)
+                        {
+                            MaximizeIcon.Data = Geometry.Parse("M2048 2048v-2048h-2048v2048h2048zM1843 1843h-1638v-1638h1638v1638z");
+                            hostWindow.Padding = new Thickness(0);
+                        }
+                        if (s == WindowState.Maximized)
+                        {
+                            MaximizeIcon.Data = Geometry.Parse("M2048 1638h-410v410h-1638v-1638h410v-410h1638v1638zm-614-1024h-1229v1229h1229v-1229zm409-409h-1229v205h1024v1024h205v-1229z");
+                            hostWindow.Padding = new Thickness(7);
+                        }
+                    });
                 }
             });
         }
 
         private void MinimizeButtonClick(object? sender, RoutedEventArgs e)
         {
-            Window? hostWindow = (Window?)VisualRoot;
-
-            if (hostWindow != null)
-                hostWindow.WindowState = WindowState.Minimized;
+            if (hostWindow == null) return;
+            hostWindow.WindowState = WindowState.Minimized;
         }
 
         private void MaximizeButtonClick(object? sender, RoutedEventArgs e)
         {
-            Window? hostWindow = (Window?)VisualRoot;
-
             if (hostWindow == null) return;
 
             if (hostWindow.WindowState == WindowState.Maximized)
@@ -105,8 +100,6 @@ namespace Promise.UI.Controls
 
         private void CloseButtonClick(object? sender, RoutedEventArgs e)
         {
-            Window? hostWindow = (Window?)VisualRoot;
-
             if (hostWindow != null)
                 hostWindow.Close();
         }
--- src/Promise.UI/Views/NotesView.axaml.cs
+++ src/Promise.UI/Views/NotesView.axaml.cs
@@ -1,13 +1,57 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.ReactiveUI;
+using Avalonia.VisualTree;
 using Promise.Application.ViewModels;
+using ReactiveUI;
+using System;
 
 namespace Promise.UI.Views
 {
     public partial class NotesView : ReactiveUserControl<NotesViewModel>
     {
+        private const double EdgeOffset = 20;
+        private Window? hostWindow;
+
         public NotesView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                hostWindow = this.GetVisualRoot() as Window;
+                if(hostWindow != null)
+                {
+                    hostWindow.Resized += HostWindowResized;
+                }
+            });
+        }
+
+        private void HostWindowResized(object? sender, WindowResizedEventArgs e)
+        {
+            double currentWidth = MainGrid.ColumnDefinitions[0].Width.Value;
+            double maxWidth = e.ClientSize.Width - EdgeOffset;
+            double newWidth = Math.Min(currentWidth, maxWidth);
+
+            if (Math.Abs(newWidth - currentWidth) > 0)
+            {
+                MainGrid.ColumnDefinitions[0].Width = new GridLength(newWidth);
+            }
+        }
+
+        private void GridSplitterDragDelta(object? sender, VectorEventArgs e)
+        {
+            if (hostWindow != null)
+            {
+                double maxWidth = hostWindow.ClientSize.Width - EdgeOffset;
+                double newWidth = MainGrid.ColumnDefinitions[0].Width.Value + e.Vector.X;
+
+                if (newWidth > 0 && newWidth <= maxWidth)
+                {
+                    MainGrid.ColumnDefinitions[0].Width = new GridLength(newWidth);
+                }
+            }
         }
     }
 }

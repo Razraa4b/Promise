4ec29f7d3f043baa1e3d3c0ce573696f506717a4
Razraaab <razraaab@gmail.com>
2025-09-26 22:31:48 +0300

    feat(saving data): added saving data about the contents of the note to the database, every 1 second if the data changes.

--- src/Promise.Application/ViewModels/NotesViewModel.cs
+++ src/Promise.Application/ViewModels/NotesViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace Promise.Application.ViewModels
 {
@@ -24,9 +25,28 @@ namespace Promise.Application.ViewModels
         public Note? SelectedNote
         {
             get => selectedNote;
-            set => this.RaiseAndSetIfChanged(ref selectedNote, value);
+            set
+            {
+                this.RaiseAndSetIfChanged(ref selectedNote, value);
+                this.RaisePropertyChanged(nameof(SelectedNoteContent));
+            }
         }
 
+        public string? SelectedNoteContent
+        {
+            get => selectedNote?.Content;
+            set
+            {
+                if (SelectedNote == null || value == null) return;
+
+                SelectedNote.Content = value;
+                isNoteContentChanged = true;
+            }
+        }
+
+        private bool isNoteContentChanged;
+
+        public ReactiveCommand<Unit, Unit> ScheduleContentSaveCommand { get; init; }
         public ReactiveCommand<string, Unit> CreateNoteCommand { get; init; }
         public ReactiveCommand<Unit, Unit> DeleteNoteCommand { get; init; }
 
@@ -36,12 +56,17 @@ namespace Promise.Application.ViewModels
             _logger = logger;
             _unitOfWork = unitOfWork;
 
+            ScheduleContentSaveCommand = ReactiveCommand.CreateFromTask(ScheduleContentSave);
             CreateNoteCommand = ReactiveCommand.CreateFromTask<string, Unit>(CreateNote);
             DeleteNoteCommand = ReactiveCommand.CreateFromTask(DeleteNote);
 
             this.WhenActivated(async (CompositeDisposable disposables) =>
             {
                 await LoadNotes();
+
+                Observable.Timer(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1))
+                    .Subscribe(x => Task.Run(ScheduleContentSave))
+                    .DisposeWith(disposables);
 
                 Disposable.Create(Notes.Clear).DisposeWith(disposables);
             });
@@ -53,9 +78,31 @@ namespace Promise.Application.ViewModels
             Notes.AddRange(notes);
         }
 
+        private async Task ScheduleContentSave()
+        {
+            if (SelectedNote == null || !isNoteContentChanged) return;
+
+            isNoteContentChanged = false;
+            try
+            {
+                await _unitOfWork.Begin();
+                await _unitOfWork.NoteRepository.Update(SelectedNote);
+                await _unitOfWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogDebug($"Occursed exception: {ex.Message}");
+                await _unitOfWork.Rollback();
+            }
+        }
+
         private async Task<Unit> CreateNote(string title)
         {
-            Note note = new Note(title, "");
+            Note note = new Note(title, "")
+            {
+                CreationTime = DateTime.Now,
+                LastChangedTime = DateTime.Now
+            };
 
             try
             {
@@ -63,8 +110,9 @@ namespace Promise.Application.ViewModels
                 await _unitOfWork.NoteRepository.Add(note);
                 await _unitOfWork.Commit();
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogDebug($"Occursed exception: {ex.Message}");
                 await _unitOfWork.Rollback();
             }
 
@@ -86,8 +134,9 @@ namespace Promise.Application.ViewModels
                 await _unitOfWork.NoteRepository.Delete(note);
                 await _unitOfWork.Commit();
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogDebug($"Occursed exception: {ex.Message}");
                 await _unitOfWork.Rollback();
             }
 
